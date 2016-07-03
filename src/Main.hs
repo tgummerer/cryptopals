a8031e18c6ff0150dd0830cfe0369ac07fcd93b7
Thomas Gummerer <t.gummerer@gmail.com>
2016-07-03 01:11:12 +0100

    use hspec for testing

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
-import UnlimitedBits as U
-import Numeric
+import Test.Hspec
+import UnlimitedBits
+
 
 main :: IO ()
-main = do
-  contents <- getContents
-  putStrLn $ toBase64  $ fromHex contents
+main = hspec $ do
+  describe  "UnlimitedBits" $ do
+    it "parses contents from hex" $ do
+      (toBase64 $ fromHex "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") `shouldBe` "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
