dd396bb3a94aa1cc221b6aa1d8510aaf509290c8
Thomas Gummerer <t.gummerer@gmail.com>
2016-07-04 22:51:28 +0100

    better scoring algorithm

--- src/XorCrypt.hs
+++ src/XorCrypt.hs
@@ -8,8 +8,16 @@ import Data.Ord
 import qualified Data.List as L
 import qualified Data.Map as M
 
+isValidChar :: Char -> Bool
+isValidChar c = c == '\n' || (ord c) > 31 && (ord c) < 127
+
+isScoringChar :: Char -> Bool
+isScoringChar c = (ord c) >= 65 && (ord c) <= 90
+                  || (ord c) >= 97 && (ord c) <= 122
+
 findFrequencies :: String -> M.Map Char Double
-findFrequencies xs = M.fromListWith (+) [(toLower c, 1.0) | c <- filter isLetter xs]
+findFrequencies xs = M.fromListWith (+) [(toLower c, 1) | c <- filter isScoringChar xs]
+  where
 
 -- Algorithm based on http://crypto.stackexchange.com/questions/30209/developing-algorithm-for-detecting-plain-text-via-frequency-analysis
 englishFreq :: [(Char, Double)]
@@ -21,15 +29,13 @@ englishFreq = zip ['a'..'z'] [ 0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02
 chi2 :: String -> (String, Double)
 chi2 st = (st, computeChi2 st)
   where
-    computeChi2 st = if (length $ filter (\x -> isAscii x && isLetter x) st) < 1 then
-                       -- some high double number that won't be reached as chi
-                       -- value by a normal sentence
-                       10000000.0
-                     else
+    computeChi2 st = if (length $ filter isValidChar st) == length st then
                        foldr f 0.0 englishFreq
+                     else
+                       100000000.0
     f (char, freq) acc = acc + ((freq * (fromIntegral len) - M.findWithDefault 0.0 char frequencies) ** 2) / (freq * (fromIntegral len))
     frequencies = findFrequencies st
-    len = length $ filter isLetter st
+    len = length $ filter isScoringChar st
 
 findClosestEnglishMatch :: [String] -> (String, Double)
 findClosestEnglishMatch xs = head $ L.sortBy (comparing $ snd) (map chi2 xs)
