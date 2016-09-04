0b1e698aff31a7956132f6453ed2951ed4610649
Thomas Gummerer <t.gummerer@gmail.com>
2016-09-04 20:39:46 +0100

    fix detecting ecb

--- src/AesCrypt.hs
+++ src/AesCrypt.hs
@@ -9,6 +9,7 @@ import Crypto.Cipher.AES
 import Data.Word
 import Data.List (maximumBy, sortBy)
 import Data.Ord (comparing)
+import qualified Data.Set as Set
 import UnlimitedBits (fromHex)
 
 decryptAesEcb :: [Word8] -> String -> B.ByteString
@@ -21,8 +22,17 @@ matches16bits :: Word8 -> Word8 -> [Word8] -> Integer
 matches16bits _ _ [] = 0
 matches16bits a b (x:y:xs) = if a == x && b == y then matches16bits a b xs + 1 else matches16bits a b xs
 
-numSameBits :: [Word8] -> Integer
-numSameBits xs = foldr (\(x,y) acc -> acc + matches16bits x y xs) 0 (pairs xs)
+findBlocks :: [Word8] -> Set.Set B.ByteString
+findBlocks [] = Set.empty
+findBlocks xs = Set.insert (B.pack $ take 16 xs) $ findBlocks (drop 16 xs)
+
+isAesEcb :: [Word8] -> Bool
+isAesEcb xs = if (fromIntegral $ Set.size (findBlocks xs)) < (fromIntegral (fromIntegral (length xs)) / 16)
+              then True
+              else False
+
+numSameBlocks :: [Word8] -> Int
+numSameBlocks xs = (length xs) `div` 16 - Set.size (findBlocks xs)
 
 detectAesEcb :: [String] -> String
-detectAesEcb xs = snd $ maximumBy (comparing fst) (zip (map (numSameBits . fromHex) xs) xs)
+detectAesEcb xs = snd $ maximumBy (comparing fst) (zip (map (numSameBlocks . fromHex) xs) xs)
